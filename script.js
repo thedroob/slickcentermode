1b904d080a9b6d6f4ae4cda0e24ec9f9ee0d2adc
Andrew Click <91501457+thedroob@users.noreply.github.com>
2021-11-04 20:14:42 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(function() {
+      $('.center').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        infinite: true,
+        dots: true,
+        speed: 300,
+      });
+    });
+
